e867d3d5cdf7aa8801225cd56ffe74cb57b68a5e
rohitannamreddy <33402645+rohitannamreddy@users.noreply.github.com>
2018-07-01 12:43:38 -0500

    Create deploy.sh

---
+++ Scripts/deploy.sh
@@ -0,0 +1,2 @@
+mkdir -p /myapp
+aws s3 cp s3://rohitanamreddys3bucket/Artifacts/sample_artifact.zip/f531c5b6-0ebf-47fd-9598-673437240dc6/sample_project_with_test_deploy/target/my-app-1.0-SNAPSHOT.jar /my-app-1.0-SNAPSHOT.jar
