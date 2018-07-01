3b2f6131bebe0f98265bcf35fb6cd69527e59623
rohitannamreddy <33402645+rohitannamreddy@users.noreply.github.com>
2018-07-01 13:39:12 -0500

    Update deploy.sh

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,2 +1,3 @@
 mkdir -p /myapp
-aws s3 cp s3://rohitanamreddys3bucket/Artifacts/sample_artifact.zip/f531c5b6-0ebf-47fd-9598-673437240dc6/sample_project_with_test_deploy/target/my-app-1.0-SNAPSHOT.jar /my-app-1.0-SNAPSHOT.jar
+aws s3 cp s3://rohitanamreddys3bucket/Artifacts/sample_artifact/sample_project_with_test_deploy /tmp/myapp.zip
+unzip /tmp/myapp.zip
